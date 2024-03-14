c301ca4fc92f760e2953ff64a5ecc9637d3260cf
PedroCorroDev <pedrocorrodev@proton.me>
2024-03-14 17:21:03 +0100

    Primer proyecto de react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
+//Proyecto finalizado
 
 function App() {
 
