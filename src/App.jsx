d12c68036e1e4d31d5248bc441f9a2d06e3fe83b
PedroCorroDev <pedrocorrodev@proton.me>
2024-03-14 17:05:47 +0100

    Cambios finales

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ import ListadoPacientes from "./components/ListadoPacientes"
 function App() {
 
   const [pacientes, setPacientes] = useState([]);
-  const [paciente, setPaciente] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
   useEffect(() => {
     const obtenerLS = () => {
-          const pacientesLS = JSON.parse(localSotrage.getItem('pacientes')) ?? []; 
+          const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []; 
           setPacientes(pacientesLS)
     }
     obtenerLS();
   }, [])
 
   useEffect(() => {
-    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
   }, [pacientes])
 
   const eliminarPaciente = (id) => {
